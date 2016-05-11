293d9b0f8b100acfcefcb3426e656e901adcdfe2
MateuszC100 <mateuszcierniak102@o2.pl>
2016-05-11 11:13:24 +0200

    Add app.spec.js

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -16,9 +16,32 @@ describe('app', function () {
         });
         it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
-                name: 'Zack', age: 5, course: 'kids'
+                name: 10, age: 5, course: 'kids'
             });
         });
+        it('This should give you an error if we give 18 to a group of children', function(){
+            expect(app.assignToSwimmingCourse('Name','10/04/1997')).toEqual({
+                name: 'Name', age: 18, course: 'kids'
+            });
+        });
+    });
+    describe('calculateArea',function(){
+        it('should message error if area < 0', function(){
+            expect(app.calculateArea(3,5,4,'Success','Error')).toEqual({
+                area: -7, message: 'Error'
+            });
+        });
+        it('should message success if area > 0',function(){
+            expect(app.calculateArea(3,2,1,'Success','Error')).toEqual({
+                area: 1, message: 'Success'
+            });
+        });
+        it('should message Big null if area: 0',function(){
+            expect(app.calculateArea(4,2,2,'Success','Area')).toEqual({
+                area: 0, message: 'Big null'
+            });
+        });
+
     });
 });
 
